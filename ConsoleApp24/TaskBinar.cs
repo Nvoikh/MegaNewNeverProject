4fedacf205e57fc870071b0c7740c82756b2a082
Nvoikh <120413793+Nvoikh@users.noreply.github.com>
2025-01-28 16:11:53 +0200

    Add files via upload

---
+++ ConsoleApp24/TaskBinar.cs
@@ -0,0 +1,83 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json.Nodes;
+using MessagePack;
+
+namespace ConsoleApp23
+{
+    public class TaskManager
+    {
+        private List<TaskItem> tasks = new List<TaskItem>();
+        private const string FileName = "tasks.dat";
+
+        
+        public void LoadTasks()
+        {
+            if (File.Exists(FileName))
+            {
+                using (var stream = new FileStream(FileName, FileMode.Open))
+                {
+                    MessagePackSerializer.Deserialize<List<Task>>(stream);
+
+
+
+                }
+            }
+        }
+
+        
+        public void SaveTasks()
+        {
+            using (var stream = new FileStream(FileName, FileMode.Create))
+            {
+                MessagePackSerializer.Serialize(stream, tasks);
+
+            }
+        }
+
+        
+        public void AddTask(Task task)
+        {
+            tasks.Add(TaskItem);
+        }
+
+       
+        public bool RemoveTask(int taskId)
+        {
+            var task = tasks.Find(t => t.Id == taskId);
+            if (task != null)
+            {
+                tasks.Remove(task);
+                return true;
+            }
+            return false;
+        }
+
+        
+        public void ViewTasks()
+        {
+            if (tasks.Count == 0)
+            {
+                Console.WriteLine("No tasks available...");
+                return;
+            }
+
+            foreach (var task in tasks)
+            {
+                Console.WriteLine(task);
+            }
+        }
+
+        
+        public Task GetTaskById(int id)
+        {
+            return tasks.Find(t => t.Id == id);
+        }
+    }
+}
